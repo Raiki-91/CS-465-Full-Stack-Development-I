377df322a847066298c597906a82789d8e910fdc
Lee Kitchen <spatulaclan@gmail.com>
2025-01-25 20:25:19 -0500

    Module 3 completed baseline

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,6 +1,11 @@
-/* GET Travel view */
+var fs = require('fs');
+var trips = JSON.parse(fs.readFileSync('./data/trips.json',
+'utf8'));
+
+/*GET travel view. */
+
 const travel = (req, res) => {
-    res.render('travel', {title: 'Travlr Getaways'});
+    res.render('travel', { title: ' Travlr Geaways, trips'});
 };
 
 module.exports = {
